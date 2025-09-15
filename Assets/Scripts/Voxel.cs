dd5fcdb110869bee33320a8f36f01e02396660bb
ybsakura0104 <146091853+ybsakura0104@users.noreply.github.com>
2025-09-15 11:01:41 +0900

    3주차 복셀 생성 2단계

--- Assets/Scripts/Voxel.cs
+++ Assets/Scripts/Voxel.cs
@@ -8,8 +8,13 @@ public class Voxel : MonoBehaviour
 {
     //복셀이 날아갈 속도 속성
     public float speed = 5;
-    void Start()
+    //일정 시간이 지나면 복셀 제거
+    //필요속성:복셀을 제거할 시간, 경과시간
+    public float destoryTime = 3.0f;
+    float currentTime;
+    void OnEnable()
     {   
+        currentTime=0;
         //랜덤한 방향 찾기
         Vector3 direction = Random.insideUnitSphere; //크기가 1이고 방향만 존재함
         //랜덤한 방향으로 날아가는 속도주기
@@ -19,10 +24,7 @@ public class Voxel : MonoBehaviour
 
     }
 
-    //일정 시간이 지나면 복셀 제거
-    //필요속성:복셀을 제거할 시간, 경과시간
-    public float destoryTime = 3.0f;
-    float currentTime;
+
     void Update()
     {
         //일정 시간이 지나면 복셀 제거
@@ -31,9 +33,12 @@ public class Voxel : MonoBehaviour
         //제거 시간이 됨
         //만약 경과 시간이 제거 시간을 초과했다면
         if (currentTime>destoryTime)
-        {
+        {   
+            gameObject.SetActive(false); //자기 자신을 비활성화화
+            
+            VoxelMaker.voxelPool.Add(gameObject);//오브젝트 풀에 다시 넣어줌
             //복셀제거
-            Destroy(gameObject);
+            // Destroy(gameObject);
         }
 
         
--- Assets/Scripts/VoxelMaker.cs
+++ Assets/Scripts/VoxelMaker.cs
@@ -6,12 +6,28 @@ public class VoxelMaker : MonoBehaviour
 {   
     //생성할 대상 등록
     public GameObject voxelFactory;
+
+    //오브젝트 풀의 크기
+    public int voxelPoolSize = 20;
+
+    //오브젝트 풀
+    //static
+    public static List<GameObject> voxelPool = new List<GameObject>();
     void Start()
-    {
-        
+    {   
+        for (int i =0; i<voxelPoolSize; i++)
+        {
+            //1.복셀 공장에서 복셀 생성하기
+            GameObject voxel = Instantiate(voxelFactory);
+            // voxel.Randerer = 랜덤한 색
+            //2.복셀 비활성화
+            voxel.SetActive(false);
+            //3.복셀 오브젝트 풀에 담기
+            voxelPool.Add(voxel);
+        }
+
     }
 
-    // Update is called once per frame
     void Update()
     {   
         //1.사용자가 마우스 클릭하면
@@ -21,11 +37,14 @@ public class VoxelMaker : MonoBehaviour
             RaycastHit hitInfo = new RaycastHit();
             //2.마우스의 위치가 바닥 위에 위치해 있다면
             if (Physics.Raycast(ray, out hitInfo))
-            {
-                //3.복셀 공장에서 복셀을 만들어야 한다.
-                GameObject voxel = Instantiate(voxelFactory);
-                //4.복셀을 배치하고 싶다
-                voxel.transform.position = hitInfo.point;
+            {   
+                if(voxelPool.Count>0) //오브젝트 풀 안에 voxel이 있는지 확인인
+                {
+                    GameObject voxel = voxelPool[0];            //오브젝트 풀 최상단의 값을 가져옴옴
+                    voxel.SetActive(true);                      //객체를 활성화함
+                    voxel.transform.position = hitInfo.point;   //RayCast를 통해 얻은 충돌지점의 위치로 객체를 이동동
+                    voxelPool.RemoveAt(0);                      //오브젝트 풀에서 Voxel 1개 제거거
+                }
             }
         }
     }
