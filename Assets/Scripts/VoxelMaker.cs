fb18e500cbf6377c1ee2259734c6f847ba812287
ybsakura0104 <146091853+ybsakura0104@users.noreply.github.com>
2025-09-22 11:12:42 +0900

    vr입력대응하기기

--- Assets/Scripts/VoxelMaker.cs
+++ Assets/Scripts/VoxelMaker.cs
@@ -25,8 +25,8 @@ public class VoxelMaker : MonoBehaviour
         {
             //1.복셀 공장에서 복셀 생성하기
             GameObject voxel = Instantiate(voxelFactory);
-    // voxel.Renderer = 랜덤한 색
-    voxel.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value); //랜덤한 색상 적용
+            // voxel.Renderer = 랜덤한 색
+            voxel.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value); //랜덤한 색상 적용
 
             //2.복셀 비활성화
             voxel.SetActive(false);
@@ -35,29 +35,39 @@ public class VoxelMaker : MonoBehaviour
         }
     }
 
+    //크로스 헤어 변수
+    public Transform crosshair;
     void Update()
     {
-        // 생성 시간마다 복셀을 만들고 싶다.
-        // 1. 경과 시간이 흐른다.
-        currentTime += Time.deltaTime;
-        // 2. 경과 시간이 생성 시간을 초과했다면
-        if (currentTime > createTime)
+        ARAVRInput.DrawCrosshair(crosshair);
+        
+        if(ARAVRInput.Get(ARAVRInput.Button.One))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitInfo = new RaycastHit();
-            //3. Ray를 쏜다.
-            if (Physics.Raycast(ray, out hitInfo))
+            //크로스헤어 그리기
+            
+            // 생성 시간마다 복셀을 만들고 싶다.
+            // 1. 경과 시간이 흐른다.
+            currentTime += Time.deltaTime;
+            // 2. 경과 시간이 생성 시간을 초과했다면
+            if (currentTime > createTime)
             {
-                // 복셀 오브젝트 풀 이용하기
-                // 1. 만약 오브젝트 풀에 복셀이 있다면
-                if(voxelPool.Count > 0)
+                // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                Ray ray = new Ray(ARAVRInput.RHandPosition,ARAVRInput.RHandDirection);
+                RaycastHit hitInfo = new RaycastHit();
+                //3. Ray를 쏜다.
+                if (Physics.Raycast(ray, out hitInfo))
                 {
-                    GameObject voxel = voxelPool[0];  //오브젝트 풀 최상단의 값을 가져옴
-                    voxel.SetActive(true);             //객체를 활성화함
-                    voxel.transform.position = hitInfo.point; //RayCast를 통해 얻은 충돌지점의 위치로 객체를 이동
-                    voxelPool.RemoveAt(0);             //오브젝트 풀에서 Voxel 1개 제거
-                    // 복셀을 생성했을 때만 경과 시간을 초기화 해줬다.
-                    currentTime = 0;
+                    // 복셀 오브젝트 풀 이용하기
+                    // 1. 만약 오브젝트 풀에 복셀이 있다면
+                    if(voxelPool.Count > 0)
+                    {
+                        GameObject voxel = voxelPool[0];  //오브젝트 풀 최상단의 값을 가져옴
+                        voxel.SetActive(true);             //객체를 활성화함
+                        voxel.transform.position = hitInfo.point; //RayCast를 통해 얻은 충돌지점의 위치로 객체를 이동
+                        voxelPool.RemoveAt(0);             //오브젝트 풀에서 Voxel 1개 제거
+                        // 복셀을 생성했을 때만 경과 시간을 초기화 해줬다.
+                        currentTime = 0;
+                    }
                 }
             }
         }
